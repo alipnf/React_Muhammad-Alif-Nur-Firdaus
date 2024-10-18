f4ba8cf7e5a18fac6779c5fc797f7482aca555ed
alipnf <alipnf@proton.me>
2024-10-18 20:43:24 +0700

    add login & register but Error hehe

--- 16_React-Form/praktikum/Create-Product/src/App.jsx
+++ 16_React-Form/praktikum/Create-Product/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./page/LandingPage";
 import CreateProduct from "./page/CreateProduct";
 import ProductDetail from "./page/ProductDetail";
+import Register from "./page/Register";
+import Login from "./page/Login";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/create-product" element={<CreateProduct />} />
         <Route path="/account/:id" element={<ProductDetail />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
