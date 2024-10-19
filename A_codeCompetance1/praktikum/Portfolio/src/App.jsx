9d0429eb9cf74c61cecc319d5fba099751479f34
alipnf <alipnf@proton.me>
2024-10-19 18:55:34 +0700

    fix: add routing

--- A_codeCompetance1/praktikum/Portfolio/src/App.jsx
+++ A_codeCompetance1/praktikum/Portfolio/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import AboutUs from "./pages/AboutUs";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
