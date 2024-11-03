ad43dc7a4bc125abb41f85285bc9b03c6666ec65
alipnf <alipnf@proton.me>
2024-11-03 16:35:21 +0700

    fix: refactor InputChat component styles
    Reimplement InputChat component styles to align with the overall design.

--- 22_Perkenalan_AI/praktikum/chatBot/src/components/InputChat.jsx
+++ 22_Perkenalan_AI/praktikum/chatBot/src/components/InputChat.jsx
@@ -1,9 +1,8 @@
 export default function InputChat({ handleSendMessage, setInput }) {
   return (
-    <div className="bottom-0 mt-4 flex">
+    <div className=" mt-4 flex w-full">
       <input
         type="text"
-        // value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Type here"
         className="input input-bordered mr-2 w-full"
